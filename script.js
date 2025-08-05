a96c08bbeaf23fbe9eaad5d839aae96cdf937264
SENA CDM Etapa Productiva <sena.etapa.productiva.cdm@gmail.com>
2025-08-04 21:06:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function guardarNombre() {
   const ficha = document.getElementById("numero-ficha").value.trim();
   const programa = document.getElementById("nombre-programa").value.trim();
   const correo = document.getElementById("correo-usuario").value.trim();
-  const instructor = document.getElementById("nombre-instructor").value.trim();
+  const instructor = document.getElementById("instructor").value.trim();
   const autorizacion = document.getElementById("autorizacion").checked;
   const tyc = document.getElementById("tyc").checked;
 
